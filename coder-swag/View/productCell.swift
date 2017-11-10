bb546d05ec7cf04e2e3cdad60b5532912baf8213
ianthehuman <imavalue@gmail.comgit commit --amend --reset-authorgit config --global user.name ianthehumangit config --global user.email imavalue@gmail.com>
2017-11-09 15:03:14 -1000

    added-collecton

---
+++ coder-swag/View/productCell.swift
@@ -0,0 +1,16 @@
+//
+//  productCellCollectionViewCell.swift
+//  coder-swag
+//
+//  Created by Ian Value on 11/9/17.
+//  Copyright © 2017 intoTheElements. All rights reserved.
+//
+
+import UIKit
+
+class productCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
